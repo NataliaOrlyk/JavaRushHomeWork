a5d5e480ac512683c3ae5b4cf192010217dd4f2c
Natalia Orlyk <NOrlyk@hrk.langate.internal>
2016-02-09 16:08:54 +0200

    First commit

---
+++ src/com/javarush/test/level00/lesson00/task04/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level00.lesson00.task04;
+
+/* Амиго очень умный
+Написать программу, выводящую на экран надпись «Амиго очень умный».
+Пример вывода на экран:
+Амиго очень умный
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Амиго очень умный");
+    }
+}
